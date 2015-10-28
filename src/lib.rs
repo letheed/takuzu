90496e547f126bf2bc26592f3836f3e4f45d53d1
Letheed <dev@daweb.se>
2015-10-28 22:17:56 +0100

    help flag; moved functions from lib to bin

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 //! A Takuzu (a.k.a. Binairo) solving library.
 //!
-//! See [tackle](../tackle/index.html) for the solver.
+//! If you are looking for a binary, see [tackle](../tackle/index.html)
+//! for an example of implementation.
 //!
 //! # About
 //!
-//! Takuzu is a number puzzle.
+//! Takuzu is a number puzzle, sometimes called binary sudoku.
 //! The objective is to fill a grid with `0`s and `1`s while
 //! observing the following rules:
 //!
@@ -14,6 +15,7 @@
 //!
 //! The grids are squares of even length.
 //! A valid grid must have one and only one solution.
+//! The solver will find and return all the valid solutions though.
 //!
 //! # Format
 //!
@@ -25,62 +27,14 @@
 //!
 //! ## Sources
 //!
-//! Check out the [Repository](https://github.com/Letheed/takuzu)
+//! Check out the [repository](https://github.com/Letheed/takuzu).
 
 
 #![warn(missing_docs)]
 #![feature(slice_patterns)]
-
-extern crate libc;
-
-use std::io::{stderr, Write};
 
 pub use grid::{Array, Grid};
 pub use source::Source;
 
 mod grid;
 mod source;
-
-/// Returns `true` if `stdout` is a terminal.
-pub fn isatty_stdout() -> bool {
-    match unsafe { libc::isatty(libc::STDOUT_FILENO) } {
-        1 => true,
-        _ => false,
-    }
-}
-
-/// Main routine for solving a grid from a source.
-///
-/// Reads a grid from a source, triggers the solving algorithm and prints
-/// the result with colors if appropriate (if `stdout` is a terminal).
-///
-/// If an error was found, prints it on `stderr` and returns.
-pub fn solve_from(source: &mut Source) {
-    let grid = match source.source() {
-        Ok(grid) => grid,
-        Err(err) => {
-            write!(stderr(), "Error: {}\n", err.0).unwrap();
-            return
-        },
-    };
-    let solutions = grid.solve();
-    if solutions.len() == 0 { write!(stderr(), "no solution\n").unwrap(); }
-    else if solutions.len() == 1 {
-        if isatty_stdout() { print!("{}", solutions[0].to_string_diff(&grid)); }
-        else { print!("{}", solutions[0]); }
-    }
-    else {
-        if isatty_stdout() {
-            write!(stderr(), "solution 1\n").unwrap();
-            print!("{}", solutions[0].to_string_diff(&grid));
-            for (i, sol) in solutions.into_iter().enumerate().skip(1) {
-                write!(stderr(), "\nsolution {}", i + 1).unwrap();
-                print!("\n{}", sol.to_string_diff(&grid));
-            }
-        }
-        else {
-            print!("{}", solutions[0]);
-            for sol in solutions.into_iter() { print!("\n{}", sol); }
-        }
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,83 @@
 //!
 //! ```shell
 //! tackle [FILE]...
+//! tackle {--help | --version}
 //! ```
 //!
-//! If no `FILE` is provided, or if `FILE` is '`-`', will read from standard input.
+//! If no `FILE` is provided, or if `FILE` is '`-`', reads from standard input.
 
+extern crate libc;
 extern crate takuzu;
 
 use std::fs::File;
 use std::io::{stderr, stdin, Write};
-use takuzu::solve_from;
+
+use takuzu::Source;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const USAGE_STRING: &'static str =
+r#"Usage: tackle [FILE]...
+       tackle {--help | --version}
 
-fn takle(filename: &String) {
+If no FILE is provided, or if FILE is "-", read from standard input.
+
+Options:
+    --help       display this message and exit
+    --version    display the version and exit
+"#;
+
+/// Returns `true` if `stdout` is a terminal.
+pub fn isatty_stdout() -> bool {
+    match unsafe { libc::isatty(libc::STDOUT_FILENO) } {
+        1 => true,
+        _ => false,
+    }
+}
+
+/// Main routine for solving a grid from a source.
+///
+/// Reads a grid from a source, triggers the solving algorithm and prints
+/// the result with colors if appropriate (if `stdout` is a terminal).
+/// If there is more than one solution, the grids are separated by
+/// an empty line and preceded by a numbered label.
+///
+/// # Failure
+///
+/// If an error was found, prints it on `stderr` and returns.
+pub fn solve_from(source: &mut Source) {
+    let grid = match source.source() {
+        Ok(grid) => grid,
+        Err(err) => {
+            write!(stderr(), "error: {}\n", err.0).unwrap();
+            return
+        },
+    };
+    let solutions = grid.solve();
+    if solutions.len() == 0 { write!(stderr(), "no solution (bug? I guess I owe you a cookie)\n").unwrap(); }
+    else if solutions.len() == 1 {
+        if isatty_stdout() { print!("{}", solutions[0].to_string_diff(&grid)); }
+        else { print!("{}", solutions[0]); }
+    }
+    else {
+        if isatty_stdout() {
+            println!("solution 1");
+            print!("{}", solutions[0].to_string_diff(&grid));
+            for (i, sol) in solutions.into_iter().enumerate().skip(1) {
+                println!("\nsolution {}", i + 1);
+                print!("{}", sol.to_string_diff(&grid));
+            }
+        }
+        else {
+            print!("{}", solutions[0]);
+            for sol in solutions.into_iter() { print!("\n{}", sol); }
+        }
+    }
+}
+
+/// Opens a file (or stdin) and feeds it to the beast.
+fn takle(filename: &String, print_filename: bool) {
     if filename == "-" {
+        if print_filename { println!("-"); }
         solve_from(&mut stdin());
     }
     else {
@@ -28,23 +91,27 @@ fn takle(filename: &String) {
                 return
             }
         };
+        if print_filename { println!("{}", filename); }
         solve_from(&mut file);
     }
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
+    if args.contains(&"--help".to_owned()) {
+        print!("{}", USAGE_STRING);
+        return
+    }
     if args.contains(&"--version".to_owned()) {
-        println!("takle (takuzu) v{}", VERSION);
+        println!("takle (takuzu) {}", VERSION);
         return
     }
     if args.len() == 0 { solve_from(&mut stdin()); }
     else {
-        if args.len() > 1 { println!("{}", &args[0]); }
-        takle(&args[0]);
+        takle(&args[0], args.len() > 1);
         for filename in args.iter().skip(1) {
-            println!("\n{}", filename);
-            takle(filename);
+            print!("\n");
+            takle(filename, true);
         }
     }
 }

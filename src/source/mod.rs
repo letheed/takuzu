e766fa271b429a4a058194a2845e69927ae8d490
letheed <dev@zaph.ch>
2017-01-25 02:47:30 +0100

    `Source` doesn't need the `Read` trait bound

--- src/source/mod.rs
+++ src/source/mod.rs
@@ -9,37 +9,43 @@ use std::io::Read;
 
 pub mod error;
 
-impl<T: ?Sized + Read> Source for T {}
-
+/// The `Source` trait allows to use any implementor as an input source.
+///
+/// # `Read` source
+///
 /// The `Source` trait allows to use any implementor of the `Read` trait
-/// as an input source for the grid string format with no additional effort.
-pub trait Source: Read {
-    /// Creates a `Grid` from a readable source.
-    ///
-    /// Reads from the source until EOF, parses the data as a string,
-    /// checking that the size is correct, then decodes it and returns `Grid`.
-    ///
-    /// # Errors
-    ///
-    /// Returns an error if either the read failed,
-    /// a character other than `0`, `1`, `.` or `\n` was found,
-    /// or the array isn't properly sized.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// # use std::error::Error;
-    /// # use std::io;
-    /// # use std::io::Write;
-    /// # use takuzu::Source;
-    /// let grid = match io::stdin().source() {
-    ///     Ok(grid) => grid,
-    ///     Err(err) => {
-    ///         write!(io::stderr(), "error: {}\n", err.description()).unwrap();
-    ///         return
-    ///     },
-    /// };
-    /// ```
+/// with no additional effort.
+/// It reads from the source until EOF, parses the data as a string,
+/// checking that the size is correct, then decodes it and returns `Grid`.
+///
+/// ## Errors
+///
+/// Returns an error if either the read failed,
+/// a character other than `0`, `1`, `.` or `\n` was found,
+/// or the array isn't properly sized.
+///
+/// ## Examples
+///
+/// ```rust
+/// # use std::error::Error;
+/// # use std::io;
+/// # use std::io::Write;
+/// # use takuzu::Source;
+/// let grid = match io::stdin().source() {
+///     Ok(grid) => grid,
+///     Err(err) => {
+///         writeln!(io::stderr(), "error: {}", err).unwrap();
+///         return
+///     },
+/// };
+/// ```
+pub trait Source {
+    /// Creates a `Grid` from a source.
+    fn source(&mut self) -> Result<Grid, SourceError>;
+}
+
+impl<T: ?Sized> Source for T
+    where T: Read {
     fn source(&mut self) -> Result<Grid, SourceError> {
         let mut buffer = String::new();
         self.read_to_string(&mut buffer)?;
